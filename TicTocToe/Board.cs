2bd2d8cde844827add55fa0d767924d468c13f1e
Davood Fazelpour <131183114+DFazelpour@users.noreply.github.com>
2024-03-13 13:38:28 +0330

    Add project files.

---
+++ TicTocToe/Board.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTocToe
+{
+    internal class Board
+    {
+        private string[] _board;
+        public Board()
+        {
+            _board = new string[9];
+            InitializeBoard();
+        }
+
+        private void InitializeBoard()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                _board[i] = "-";
+            }
+        }
+
+        public void PrintBoard()
+        {
+            for (int i = 0; i < _board.Length; i++)
+            {
+                Console.Write($"|  {_board[i]}  ");
+                if (i % 3 == 2)
+                {
+                    Console.WriteLine("|");
+                    Console.WriteLine();
+                }
+            }
+        }
+
+    }
+}
